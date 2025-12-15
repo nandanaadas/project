faf9f9fa4f3d8e128f85a207be9282dc63e72a78
nandanaadas <nandanadas3012@gmail.com>
2025-12-15 17:19:53 +0530

    Update Shoppingbill.java

--- src/project/Shoppingbill.java
+++ src/project/Shoppingbill.java
@@ -6,10 +6,10 @@ public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 
 System.out.print("Enter Product Price: ");
-double price = 10;
+double price = sc.nextDouble();
 
 System.out.print("Enter Quantity: ");
-int qty = 5;
+int qty = sc.nextInt();
 
 double total = price * qty;
 double discount = total * 0.10;
