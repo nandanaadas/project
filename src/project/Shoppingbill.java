ba614783f2aeff68d03752890961167c2f36fa3a
matlab <matlab@sjt418scope041>
2025-12-15 16:38:28 +0530

    first

---
+++ src/project/Shoppingbill.java
@@ -0,0 +1,22 @@
+package project;
+import java.util.Scanner;
+
+public class Shoppingbill {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Enter Product Price: ");
+double price = sc.nextDouble();
+
+System.out.print("Enter Quantity: ");
+int qty = sc.nextInt();
+
+double total = price * qty;
+double discount = total * 0.10;
+double finalAmount = total - discount;
+
+System.out.println("Total Amount: " + total);
+System.out.println("Discount: " + discount);
+System.out.println("Final Payable Amount: " + finalAmount);
+}
+}
